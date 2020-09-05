a53bd608579b3167edd17cc919b7d5019387eac5
aoji-tjut <245611640@qq.com>
2020-09-05 21:59:53 +0800

    file datastream textstream buffer

--- 22-File/widget.cpp
+++ 22-File/widget.cpp
@@ -9,6 +9,13 @@ Widget::Widget(QWidget* parent)
 
 	setFixedSize(600, 400);
 	setWindowTitle("File");
+	ui->textEdit->setFont(QFont("Helvetica", 20, QFont::Normal));
+
+	WriteDataStream();
+	ReadDataStream();
+	WriteTextStream();
+	ReadTextStream();
+	Buffer();
 }
 
 Widget::~Widget()
@@ -28,13 +35,13 @@ void Widget::on_bt_read_clicked()
 	}
 
 	//打开文件
-	QFile file(path);	//文件对象
+	QFile file(path);
 	if(file.open(QIODevice::ReadOnly))	//只读打开 成功返回true
 	{
 		//获取文件信息
 		QFileInfo info(file);
-		QString str = QString("文件名：%1\n文件大小：%2字节\n绝对路径：%3\n\n").
-					  arg(info.fileName()).arg(info.size()).arg(info.absolutePath());
+		QString str = QString("文件名：%1\n文件大小：%2字节\n文件路径：%3\n\n").
+					  arg(info.fileName()).arg(info.size()).arg(info.filePath());
 
 		//一次性读完
 		//QByteArray arr=file.readAll();
@@ -64,13 +71,100 @@ void Widget::on_bt_write_clicked()
 	}
 
 	//打开文件
-	QFile file(path);	//文件对象
+	QFile file(path);
 	if(file.open(QIODevice::WriteOnly))		//只写打开 成功返回true
 	{
 		QString str = ui->textEdit->toPlainText();	//获取内容
-
 		file.write(str.toUtf8());
 
 		file.close();
+	}
+}
+
+//读二进制文件
+void Widget::ReadDataStream()
+{
+	QFile file("./data.txt");
+	if(file.open(QIODevice::ReadOnly))
+	{
+		QDataStream stream(&file);
+		QString str;
+		int i;
+		stream >> str >> i;
+		cout << str.toUtf8().data() << i << endl;
+
+		file.close();
+	}
+}
+
+//写二进制文件
+void Widget::WriteDataStream()
+{
+	QFile file("./data.txt");
+	if(file.open(QIODevice::WriteOnly))
+	{
+		QDataStream stream(&file);
+		stream << QString("二进制文件") << 666;
+
+		file.close();
+	}
+}
+
+//读文本文件
+void Widget::ReadTextStream()
+{
+	QFile file("./text.txt");
+	if(file.open(QIODevice::ReadOnly))
+	{
+		QTextStream stream(&file);
+		stream.setCodec("UTF-8");	//可以指定编码
+
+		//读文本文件必须一次性读完或按行读
+		QString str = stream.readAll();
+		cout << str.toUtf8().data() << endl;
+
+		file.close();
+	}
+}
+
+//写文本文件
+void Widget::WriteTextStream()
+{
+	QFile file("./text.txt");
+	if(file.open(QIODevice::WriteOnly))
+	{
+		QTextStream stream(&file);
+		stream.setCodec("UTF-8");				//可以指定编码
+		stream << QString("文本文件") << 666;		//"文本文件"与666之间没有间隔
+
+		file.close();
+	}
+}
+
+//内存文件 类似QFile
+void Widget::Buffer()
+{
+	QByteArray arr;
+	QBuffer buf(&arr);
+
+	//写
+	if(buf.open(QIODevice::WriteOnly))
+	{
+		QTextStream stream(&buf);	//使用文本流写入buf
+		stream.setCodec("UTF-8");
+		stream << QString("内存文件") << 666;
+
+		buf.close();
+	}
+
+	//读
+	if(buf.open(QIODevice::ReadOnly))
+	{
+		QTextStream stream(&buf);	//使用文本流读取buf
+		stream.setCodec("UTF-8");	//可以指定编码
+		QString str = stream.readAll();
+		cout << str.toUtf8().data() << endl;
+
+		buf.close();
 	}
 }
--- 22-File/widget.h
+++ 22-File/widget.h
@@ -2,10 +2,17 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QString>
+#include <QDebug>
 #include <QFile>		//文件
 #include <QFileDialog>	//文件对话框
 #include <QFileInfo>	//文件信息
-#include <QString>
+#include <QDataStream>  //二进制文件流
+#include <QTextStream>  //文本文件流
+#include <QBuffer>      //内存文件
+
+//输出[文件名:行号]
+#define cout qDebug() << "[" << __FILE__ << ":" << __LINE__ << "]"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -18,6 +25,12 @@ class Widget : public QWidget
 public:
 	Widget(QWidget* parent = nullptr);
 	~Widget();
+
+	void ReadDataStream();
+	void WriteDataStream();
+	void ReadTextStream();
+	void WriteTextStream();
+	void Buffer();
 
 private slots:
 	void on_bt_read_clicked();
